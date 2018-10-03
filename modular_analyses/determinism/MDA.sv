b386e269562f36f189ec05ebfeefa392b913c298
Travis Carlson <travis.carlson@cs.umn.edu>
2018-10-03 15:24:40 -0500

    modernize Makefiles, add Jenkinsfile

--- modular_analyses/determinism/MDA.sv
+++ modular_analyses/determinism/MDA.sv
@@ -4,6 +4,7 @@ import edu:umn:cs:melt:ableC:host;
 
 copper_mda testStmt(ablecParser) {
   edu:umn:cs:melt:ableC:host;
+  edu:umn:cs:melt:exts:ableC:check;
   edu:umn:cs:melt:exts:ableC:checkTaggedUnion;
 }
 
